d01d23823cdc355a003a6c691580c36ebc27bee2
Aniket Gahiware <annygahiware04@gmail.com>
2021-07-09 13:02:53 +0530

    Day 3 done

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String name = "Aniket";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/home_page.dart';
+import 'package:flutter_app/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello World"),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      //home: LoginPage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
